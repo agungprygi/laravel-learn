6a4d02a9bb96f0e5802d6df5a1d75835ccb14637
Agung Prayogi <123200085@student.upnyk.ac.id>
2024-12-16 17:12:32 +0700

    [INIT]initialize Laravel Learn

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout title="Contact">
+    <x-slot name="heading">
+        Contact
+    </x-slot>
+    Contact
+</x-app-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout title="Home">
+    <x-slot name="heading">
+        Home
+    </x-slot>
+    Home
+</x-app-layout>
